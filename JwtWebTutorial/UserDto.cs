aea05c4e483ecea0a972f4f7f5105903867e6e20
olive <oliverbleach99@gmail.com>
2023-02-23 17:15:43 +0100

    Add project files.

---
+++ JwtWebTutorial/UserDto.cs
@@ -0,0 +1,9 @@
+﻿namespace JwtWebTutorial
+{
+    public class UserDto
+    {
+        public string UserName { get; set; } = String.Empty;
+        public string Password { get; set; } = String.Empty;
+
+    }
+}
